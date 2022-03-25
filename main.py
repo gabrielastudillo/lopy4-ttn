0cb3470aac6f0c15032b92864cdc0e12712c11d3
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2022-03-25 15:58:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import pycom
 import socket
 import ubinascii
 from pycoproc_1 import Pycoproc
-#from LIS2HH12 import LIS2HH12
+from LIS2HH12 import LIS2HH12
 from SI7006A20 import SI7006A20
-#from LTR329ALS01 import LTR329ALS01
-#from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
+from LTR329ALS01 import LTR329ALS01
+from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
 from network import LoRa
 
 pycom.heartbeat(False)
 pycom.rgbled(0x0A0A08) # white
-py = Pycoproc(Pycoproc.PYSENSE)
-si = SI7006A20(py)
+pysense = Pycoproc(Pycoproc.PYSENSE)
+mpl3115a2 = MPL3115A2() # Barometric Pressure Sensor with Altimeter
+ltr329als01 = LTR329ALS01() # Digital Ambient Light Sensor
+si7006a20 = SI7006A20() # Humidity and Temperature sensor
+lis2hh12 = LIS2HH12() # 3-Axis Accelerometer
 
 # Initialise LoRa in LORAWAN mode.
 # Please pick the region that matches where you are using the device:
@@ -53,17 +56,34 @@ print('Joined')
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 # set the LoRaWAN data rate
-s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 2)
 
 while True:
     # make the socket blocking
     # (waits for the data to be sent and for the 2 receive windows to expire)
     s.setblocking(True)
-    temperature = si.temp()
-    humidity = si.humidity()
+    # Read the values from the sensors
+    voltage = pysense.read_battery_voltage()
+    temperature = mpl3115a2.temperature()
+    pressure = mpl3115a2.pressure()
+    light = ltr329als01.light()[0]
+    humidity = si7006a20.humidity()
+    roll = lis2hh12.roll()
+    pitch = lis2hh12.pitch()
+    # Debug sensor values
+    print('voltage:{}, temperature:{}, pressure:{}, light:{}, humidity:{}, roll:{}, pitch:{}'.format(voltage, temperature, pressure, light, humidity, roll, pitch))
     # Convert to byte array for transmission
-    payload = struct.pack(">ff", temperature,humidity)
-    s.send(payload)
+    clean_bytes = struct.pack(">iiiiiii",
+        int(temperature * 100), # Temperature in celcius
+        int(pressure * 100), # Atmospheric pressure in bar
+        int(light * 100), # Light in lux
+        int(humidity * 100), # Humidity in percentages
+        int(roll * 100), # Roll in degrees in the range -180 to 180
+        int(pitch * 100), # Pitch in degrees in the range -90 to 90
+        int(voltage * 100)) # Battery voltage
+    #payload = struct.pack(">ff", temperature,humidity)
+    #s.send(payload)
+    s.send(clean_bytes)
     print("Bytes sent, sleeping for 10 secs")
     time.sleep(10)
     # make the socket non-blocking
@@ -71,10 +91,4 @@ while True:
     s.setblocking(False)
     # get any data received (if any...)
     data = s.recv(64)
-    print(data)    
-    
-# send some data
-#s.send(bytes([0x01, 0x02, 0x03]))
-
-
-
+    print(data)
